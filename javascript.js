2f0ee4a3a58ff8a14a0205c128693cea0dc0edf7
Nasma <nasma.mlamali@icloud.com>
2018-03-05 18:08:56 -0800

    update

--- javascript.js
+++ javascript.js
@@ -1,2 +1,28 @@
 console.log ("Hello");
 
+$(document).ready(function(e) {
+    
+    $('#btnValidate').click(function(e) {
+        e.preventDefault();
+
+        var sEmail = $('#txtEmail').val();
+        
+        if (validateEmail(sEmail)) {
+            alert('Email is valid');
+        }
+        else{
+            alert('Invalid Email Address');
+
+        }
+    });
+
+    function validateEmail(sEmail) {
+        var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        if (filter.test(sEmail)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+});
